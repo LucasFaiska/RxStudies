300c48b7e72f67f44f0522f4f1941b1726857452
Lucas <lucas.faiskaa@gmail.com>
2018-01-08 00:08:23 -0200

    Initial commit

---
+++ app/src/main/java/com/lfaiska/rxstudies/BasicObservableActivity.java
@@ -0,0 +1,78 @@
+package com.lfaiska.rxstudies;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import io.reactivex.Observable;
+import io.reactivex.Observer;
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.disposables.Disposable;
+import io.reactivex.schedulers.Schedulers;
+
+public class BasicObservableActivity extends AppCompatActivity {
+
+    private static final String TAG = BasicObservableActivity.class.getSimpleName();
+    Button btn;
+    TextView textView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_example);
+
+        btn = (Button) findViewById(R.id.btn);
+        textView = (TextView) findViewById(R.id.textView);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                textView.setText("");
+                doSomeWork();
+            }
+        });
+    }
+
+    private void doSomeWork() {
+        getObservable()
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(getObserver());
+    }
+
+    private Observer<String> getObserver() {
+        return new Observer<String>() {
+
+            @Override
+            public void onSubscribe(Disposable d) {
+                Log.d(TAG, " onSubscribe : " + d.isDisposed());
+            }
+
+            @Override
+            public void onNext(String value) {
+                textView.append(" onNext : value : " + value + "\n");
+                Log.d(TAG, " onNext value : " + value);
+            }
+
+            @Override
+            public void onError(Throwable e) {
+                textView.append(" onError : " + e.getMessage() + "\n");
+                Log.d(TAG, " onError : " + e.getMessage());
+            }
+
+            @Override
+            public void onComplete() {
+                textView.append(" onComplete" + "\n");
+                Log.d(TAG, " onComplete");
+
+            }
+        };
+    }
+
+    private Observable<String> getObservable() {
+        return Observable.just("Grêmio", "Internacional","Juventude","Caxias");
+    }
+}
---
+++ app/src/main/java/com/lfaiska/rxstudies/MainActivity.java
@@ -0,0 +1,19 @@
+package com.lfaiska.rxstudies;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void startBasicObservable(View view) {
+        startActivity(new Intent(MainActivity.this, BasicObservableActivity.class));
+    }
+}
